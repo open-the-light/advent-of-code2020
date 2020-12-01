8eca280e611a85e927394bf034ecb1cd8f8955b2
open-the-light <37818407+open-the-light@users.noreply.github.com>
2020-12-01 16:23:51 +0000

    Day1Done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace advent_of_code
 {
@@ -6,7 +8,44 @@ namespace advent_of_code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Day1.Solution();
+            Day1.SolutionPart2();
+        }
+    }
+
+    class Day1
+    {
+        public static void Solution()
+        {
+            double[] lines = File.ReadAllLines("./data/input_day1.txt").Select(n => double.Parse(n)).Distinct().ToArray();
+            foreach (double n in lines)
+            {
+                foreach (double m in lines)
+                {
+                    if (n + m == 2020 && n != m)
+                    {
+                        Console.WriteLine(n * m);
+                    }
+                }
+            }
+        }
+
+        public static void SolutionPart2()
+        {
+            double[] lines = File.ReadAllLines("./data/input_day1.txt").Select(n => double.Parse(n)).Distinct().ToArray();
+            foreach (double n in lines)
+            {
+                foreach (double m in lines)
+                {
+                    foreach (double l in lines)
+                    {
+                        if (n + m + l == 2020)
+                        {
+                            Console.WriteLine(n * m * l);
+                        }
+                    }
+                }
+            }
         }
     }
 }
